26ffb8a619aac2de03d544cfa5565fb6d64a6b86
Krzysztof Magiera <krzys@swmansion.com>
2026-07-06 18:20:57 +0200

    Update SimCam casks to 1.10 (29)

--- Casks/simcam@1.10.rb
+++ Casks/simcam@1.10.rb
@@ -1,4 +1,4 @@
-cask "simcam" do
+cask "simcam@1.10" do
   version "1.10,29"
   sha256 "982a042238505f413d6639bd782de869671e3e4bbf3a6abdd2f099949ba202f9"
 
@@ -7,16 +7,18 @@ cask "simcam" do
   desc "Control virtual camera of your iOS Simulator using menubar app or a CLI: stream your Mac's webcam, inject an image, or generate a QR code."
   homepage "https://simcam.swmansion.com/"
 
-  livecheck do
-    url "https://updates.simcam.swmansion.com/appcast.xml"
-    strategy :sparkle
-  end
-
-  auto_updates true
+  conflicts_with cask: "simcam"
   depends_on macos: :sequoia
 
   app "SimCam.app"
   binary "#{appdir}/SimCam.app/Contents/MacOS/simcamctl"
+
+  postflight do
+    system_command "/usr/bin/defaults",
+                   args: ["write", "com.swmansion.SimCam", "SUEnableAutomaticChecks", "-bool", "false"]
+    system_command "/usr/bin/defaults",
+                   args: ["write", "com.swmansion.SimCam", "SUAutomaticallyUpdate", "-bool", "false"]
+  end
 
   uninstall quit: "com.swmansion.SimCam"
 
